3f260c76277579593b136832c55818c41f4531f8
Shiva Sathwik <shivasathwik74@gmail.com>
2025-11-24 23:04:17 +0530

    error fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/portfolioo',
+  assetPrefix: '/portfolioo',
   images: {
     unoptimized: true,
   },

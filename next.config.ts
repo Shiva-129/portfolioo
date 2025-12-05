f81899e05db13fd1d2db1ad5b127834b019c79d5
Shiva Sathwik <shivasathwik74@gmail.com>
2025-12-05 19:19:37 +0530

    Remove assetPrefix to fix styles

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/portfolioo' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolioo' : '',
   images: {
     unoptimized: true,
   },

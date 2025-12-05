d506444b4a3d0db8df61093e1bf7117760ad93d0
Shiva Sathwik <shivasathwik74@gmail.com>
2025-12-05 19:29:58 +0530

    fixxes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/portfolioo' : '',
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
